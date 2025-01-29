daec54484fedb87103582d14847d514237531e00
Joshua Vandaële <joshua@vandaele.software>
2025-01-29 17:23:39 +0100

    Do not clear the screen by default when using SSH

--- ssh.sh
+++ ssh.sh
@@ -1,1 +1,1 @@
-ssh -i Data/unsecurekey_rsa runner@localhost -p10022 -o "UserKnownHostsFile=/dev/null" -o StrictHostKeyChecking=no -o PasswordAuthentication=no
+ssh -i Data/unsecurekey_rsa runner@localhost -p10022 -o "UserKnownHostsFile=/dev/null" -o StrictHostKeyChecking=no -o PasswordAuthentication=no -T
