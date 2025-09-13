909e47931177573a1eab6fd3356c27f41ffa9e0f
Joshua Vandaële <joshua@vandaele.software>
2025-09-13 04:39:47 +0200

    Save more space on Windows

--- Data/Unattend/Setup/Scripts/FirstLogon.ps1
+++ Data/Unattend/Setup/Scripts/FirstLogon.ps1
@@ -7,36 +7,36 @@ function Prepend-ToFile {
         [string]$FilePath
     )
 
-    $content = $TextToPrepend + "`n" + (Get-Content -Raw $FilePath)
-    Set-Content -Path $FilePath -Value $content
+    $fileContent = $TextToPrepend + "`n" + (Get-Content -Raw $FilePath)
+    Set-Content -Path $FilePath -Value $fileContent
 }
 
+function Install-OpenSshServer {
+    Add-WindowsCapability -Online -Name OpenSSH.Server
 
-function SetupSSH {
-    Add-WindowsCapability -Online -Name OpenSSH.Server;
+    Set-Service -Name sshd -StartupType Automatic
+    Set-Service -Name ssh-agent -StartupType Automatic
 
-    Set-Service -Name sshd -StartupType 'Automatic';
-    Set-Service -Name ssh-agent -StartupType 'Automatic';
+    New-NetFirewallRule -DisplayName "Allow OpenSSH Inbound" -Direction Inbound -Program "C:\Windows\System32\OpenSSH\sshd.exe" -Action Allow -Profile Any
+    New-NetFirewallRule -DisplayName "Allow OpenSSH Outbound" -Direction Outbound -Program "C:\Windows\System32\OpenSSH\sshd.exe" -Action Allow -Profile Any
 
-    New-NetFirewallRule -DisplayName "Allow OpenSSH Inbound" -Direction Inbound -Program "C:\Windows\System32\OpenSSH\sshd.exe" -Action Allow -Profile Any;
-    New-NetFirewallRule -DisplayName "Allow OpenSSH Outbound" -Direction Outbound -Program "C:\Windows\System32\OpenSSH\sshd.exe" -Action Allow -Profile Any;
+    Start-Service ssh-agent
+    Start-Service sshd
 
-    Start-Service ssh-agent;
-    Start-Service sshd;
-
-    $sshDir = "$env:USERPROFILE\.ssh"
-    if (-not (Test-Path -Path $sshDir)) {
-        mkdir $sshDir
+    $sshFolder = "$env:USERPROFILE\.ssh"
+    if (-not (Test-Path -Path $sshFolder)) {
+        New-Item -ItemType Directory -Path $sshFolder
     }
 
     Prepend-ToFile "Port 10022" "$env:ProgramData\ssh\sshd_config"
-    $authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCa89hPd0mxguYGSVhnqz2HcRkohbiYNPl+ibbdlOnxx5/w9cCDQWCaipBTC8FZAXv2bBkdgL0nzRoEL7r7F/zvYjR2FqPkLGCsDypiOS94R2CDiRTXgzo7v1AadgCLgMe3VBph78qaEiMu3buMexN/QF0VUE2rfBtDLZaOSrteWAygXALZp7frWtpQodDNkYM0K3LSvWjNG5HDPXagyNYjeSos1z/8zi0Su+syo0qjkuJY5GSBV4s6TgNJAOt9CFzTp/Q3Uts7UoL7tqUuw4W/+hf+ITErZ8NyxIQyUMH8yZEt2PQGToBwlPrshqoq3ftYQrPAVqHrason8TnYCLHToiOwkYrzoa3YCusUL/HYeUGHdocFS++nBG22ANpAOeD+h7UQliiCzRhPt2oph8vTIJ/LeSnosf3F05Nl6AXzjhTvQ5mko16xOTxq8OW3QTk3hWsmGJwlJb5zne8XQ4MWiHgeNq0iwJ5EiVHkQPgTd8Z9pzjc54iMMKR+zmYacWc= Unsecure key for use with guests. DO NOT EXPOSE THEIR SSH PORTS TO THE INTERNET, EVER"
-    $authorized_keys | Add-Content -Path "$env:ProgramData\ssh\administrators_authorized_keys"
+
+    $authorizedKeysContent = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCa89hPd0mxguYGSVhnqz2HcRkohbiYNPl+ibbdlOnxx5/w9cCDQWCaipBTC8FZAXv2bBkdgL0nzRoEL7r7F/zvYjR2FqPkLGCsDypiOS94R2CDiRTXgzo7v1AadgCLgMe3VBph78qaEiMu3buMexN/QF0VUE2rfBtDLZaOSrteWAygXALZp7frWtpQodDNkYM0K3LSvWjNG5HDPXagyNYjeSos1z/8zi0Su+syo0qjkuJY5GSBV4s6TgNJAOt9CFzTp/Q3Uts7UoL7tqUuw4W/+hf+ITErZ8NyxIQyUMH8yZEt2PQGToBwlPrshqoq3ftYQrPAVqHrason8TnYCLHToiOwkYrzoa3YCusUL/HYeUGHdocFS++nBG22ANpAOeD+h7UQliiCzRhPt2oph8vTIJ/LeSnosf3F05Nl6AXzjhTvQ5mko16xOTxq8OW3QTk3hWsmGJwlJb5zne8XQ4MWiHgeNq0iwJ5EiVHkQPgTd8Z9pzjc54iMMKR+zmYacWc= Unsecure key for guests. DO NOT EXPOSE THEIR SSH PORTS TO THE INTERNET, EVER"
+    $authorizedKeysContent | Add-Content -Path "$env:ProgramData\ssh\administrators_authorized_keys"
 
     $acl = Get-Acl "$env:ProgramData\ssh\administrators_authorized_keys"
     $acl.SetAccessRuleProtection($true, $false)
-    $administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow")
-    $systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow")
+    $administratorsRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "Allow")
+    $systemRule = New-Object System.Security.AccessControl.FileSystemAccessRule("SYSTEM", "FullControl", "Allow")
     $acl.SetAccessRule($administratorsRule)
     $acl.SetAccessRule($systemRule)
     $acl | Set-Acl
@@ -44,39 +44,79 @@ function SetupSSH {
     New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\Setup\SSHShell.bat" -PropertyType String -Force
 }
 
-function InstallWinget {
-    (Get-AppxProvisionedPackage -Online -LogLevel Warnings | Where-Object -Property DisplayName -EQ Microsoft.DesktopAppInstaller).InstallLocation -replace '%SYSTEMDRIVE%', $env:SystemDrive | Add-AppxPackage -Register -DisableDevelopmentMode
+function Install-Winget {
+    (Get-AppxProvisionedPackage -Online | Where-Object DisplayName -EQ "Microsoft.DesktopAppInstaller").InstallLocation -replace '%SYSTEMDRIVE%', $env:SystemDrive | Add-AppxPackage -Register -DisableDevelopmentMode
     winget.exe source update --disable-interactivity
 }
 
-function InstallApps {
+function Install-Applications {
     winget.exe install --id Microsoft.VisualStudio.2022.BuildTools -e --accept-package-agreements --accept-source-agreements --override "--passive --wait --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended"
     winget.exe install --id Git.Git -e --force --accept-package-agreements --accept-source-agreements
 }
 
-function CloneDolphin {
-    & "C:\Program Files\Git\bin\git.exe" clone https://github.com/dolphin-emu/dolphin.git "$env:USERPROFILE\dolphin"
-    cd "$env:USERPROFILE\dolphin"
-    & "C:\Program Files\Git\bin\git.exe" submodule update --init --recursive
+function Clone-Dolphin {
+    $gitExe = "C:\Program Files\Git\bin\git.exe"
+    $dolphinFolder = "$env:USERPROFILE\dolphin"
+    $shortcutPath = Join-Path $env:USERPROFILE "Desktop\Dolphin.lnk"
+
+    & $gitExe clone https://github.com/dolphin-emu/dolphin.git $dolphinFolder
+    & $gitExe -C $dolphinFolder submodule update --init --recursive
+
+    $wshShell = New-Object -ComObject WScript.Shell
+    $shortcut = $wshShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $dolphinFolder
+    $shortcut.WorkingDirectory = $dolphinFolder
+    $shortcut.Save()
+
+    $shellApplication = New-Object -ComObject Shell.Application
+    $shellApplication.Namespace($dolphinFolder).Self.InvokeVerb("pintohome")
+}
+
+function Remove-WindowsRecoveryPartition {
+    Disable-ComputerRestore -Drive "C:"
+
+    $partitions = Get-Partition -DiskNumber 0
+    $cPartition = $partitions | Where-Object DriveLetter -eq 'C'
+    $recoveryPartition = $partitions | Where-Object Type -eq 'Recovery'
+
+    $diskpartScript = @"
+select disk 0
+select partition $($recoveryPartition[0].PartitionNumber)
+delete partition override
+select partition $($cPartition[0].PartitionNumber)
+extend
+"@
+
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    $diskpartScript | Out-File -FilePath $tempFile -Encoding ASCII
+    diskpart /s $tempFile
+    Remove-Item -Path $tempFile -Force
+}
+
+function Set-HighPerformancePowerPlan {
+    $highPerfPlan = Get-WmiObject -Namespace root\cimv2\power -Class Win32_PowerPlan | Where-Object ElementName -eq "High performance"
+    if ($highPerfPlan) { $highPerfPlan.Activate() }
 }
 
 $scripts = @(
-    {Set-ItemProperty -LiteralPath 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name 'AutoLogonCount' -Type 'DWord' -Force -Value 0};
-    {SetupSSH};
-    {InstallWinget};
-    {InstallApps};
-    {CloneDolphin};
-    {Copy-Item -Path "C:\Windows\Setup\hosts" -Destination "C:\Windows\System32\Drivers\etc\hosts" -Force -ErrorAction Stop};
-    {Stop-Computer};
-);
+    { Set-ItemProperty -LiteralPath 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name 'AutoLogonCount' -Type DWord -Force -Value 0 },
+    { Install-OpenSshServer },
+    { Install-Winget },
+    { Install-Applications },
+    { Clone-Dolphin },
+    { Move-Item -Path "C:\Windows\Setup\hosts" -Destination "C:\Windows\System32\Drivers\etc\hosts" -Force -ErrorAction Stop },
+    { Set-HighPerformancePowerPlan },
+    { Remove-WindowsRecoveryPartition },
+    { Stop-Computer }
+)
 
 & {
-	[float] $complete = 0;
-	[float] $increment = 100 / $scripts.Count;
+	[float] $complete = 0
+	[float] $increment = 100 / $scripts.Count
 	foreach( $script in $scripts ) {
-		Write-Progress -Activity 'Running scripts to finalize your Windows installation. Do not close this window.' -PercentComplete $complete;
-        Write-Output "--- $script";
-		& $script;
-		$complete += $increment;
+		Write-Progress -Activity 'Running scripts to finalize your Windows installation. Do not close this window.' -PercentComplete $complete
+        Write-Output "--- $script"
+		& $script
+		$complete += $increment
 	}
-} *>&1 >> "C:\Windows\Setup\Scripts\FirstLogon.log";
+} *>&1 >> "C:\Windows\Setup\Scripts\FirstLogon.log"
--- Data/Unattend/Setup/Scripts/Specialize.ps1
+++ Data/Unattend/Setup/Scripts/Specialize.ps1
@@ -1,29 +1,32 @@
 $scripts = @(
-	{
-		ReAgentc.exe /disable;
-		Remove-Item -LiteralPath 'C:\Windows\System32\Recovery\Winre.wim' -Force -ErrorAction 'SilentlyContinue';
-	};
-	{
-		reg.exe add "HKLM\SYSTEM\Setup\MoSetup" /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1 /f;
-	};
-	{
-		reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v BypassNRO /t REG_DWORD /d 1 /f;
-	};
-	{
-		net.exe accounts /lockoutthreshold:0;
-	};
-	{
-		net.exe accounts /maxpwage:UNLIMITED;
-	};
-	{
-		reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
-	};
-	{
-		reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 0 /f;
-	};
-	{
-		reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\BitLocker" /v "PreventDeviceEncryption" /t REG_DWORD /d 1 /f;
-	};
+    {
+        Disable-WindowsOptionalFeature -Online -FeatureName "WinRE" -NoRestart -ErrorAction SilentlyContinue
+        Remove-Item -Recurse -LiteralPath 'C:\Windows\System32\Recovery\' -Force -ErrorAction SilentlyContinue
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SYSTEM\Setup\MoSetup" -Name "AllowUpgradesWithUnsupportedTPMOrCPU" -PropertyType DWord -Value 1 -Force
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "BypassNRO" -PropertyType DWord -Value 1 -Force
+    };
+    {
+        net.exe accounts /lockoutthreshold:0
+    };
+    {
+        net.exe accounts /maxpwage:UNLIMITED
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -PropertyType DWord -Value 0 -Force
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -PropertyType DWord -Value 0 -Force
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "PagingFiles" -PropertyType String -Value "" -Force
+    };
+    {
+        New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\BitLocker" -Name "PreventDeviceEncryption" -PropertyType DWord -Value 1 -Force
+    };
 );
 
 & {
